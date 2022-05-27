1a3a003763eff817e339d123361c28ed9c28d09d
Habiba <habebaosama14@gmail.com>
2022-05-27 05:35:05 +0200

    insertion

--- src/lab5/b_tree/BTree.java
+++ src/lab5/b_tree/BTree.java
@@ -1,31 +1,284 @@
 package lab5.b_tree;
 
+import javax.management.RuntimeErrorException;
+
+import java.util.LinkedList;
 import java.util.List;
 
 public class BTree<K extends Comparable<K>, V> implements IBTree<K, V> {
 
-    private final int t;
-    private IBTreeNode<K, V> root;
 
-    public BTree(int t) {
-        this.t = t;
+    private final int maxKeys;
+    private IBTreeNode<K, V> root;
+    private final int minimumDegree;
+
+
+    public BTree(int minimumDegree) {
         this.root = null;
+        if (minimumDegree < 1) {
+            throw new RuntimeErrorException(new Error());
+        } else {
+            this.minimumDegree = minimumDegree;
+
+            this.maxKeys = minimumDegree * 2 - 1;
+        }
     }
 
     @Override
     public int getMinimumDegree() {
-        return t;
+        return this.minimumDegree;
     }
 
     @Override
     public IBTreeNode<K, V> getRoot() {
-        return null;
+        return this.root;
     }
-
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     @Override
     public void insert(K key, V value) {
+        if (search(key) != null)//already exists
+            return;
 
+        if (this.getRoot() == null) {
+            //for the first node
+            this.root = new BTreeNode<>();
+            List<K> keys = new LinkedList<>();
+            keys.add(key);
+            List<V> values = new LinkedList<>();
+            values.add(value);
+            List<IBTreeNode<K, V>> children = new LinkedList<>();
+            this.root.setChildren(children);
+            this.root.setKeys(keys);
+            this.root.setValues(values);
+            this.root.setLeaf(true);
+            this.root.setNumOfKeys(1);
+        } else {
+            //to get the position where to insert the new key
+            IBTreeNode<K, V> insertInto = this.getInsertionPosition(this.root, key);
+            List<K> keys = insertInto.getKeys();
+            //if the inserted value is greater than the key in the list
+            //then find the right to place to insert into inside the node
+            int i;
+            for (i = 0; i < insertInto.getNumOfKeys(); i++) { //find the right to place to insert into inside the node
+                if (key.compareTo(keys.get(i)) < 0) {
+                    break;
+                }
+            }
+            keys.add(i, key);
+            insertInto.getValues().add(i, value);
+            insertInto.setNumOfKeys(keys.size());
+        }
     }
+
+    public IBTreeNode<K, V> getInsertionPosition(IBTreeNode<K, V> node, K key) {
+        // If root is full, then tree grows in height
+        //so we need to split it
+        if (node.getNumOfKeys() == this.maxKeys) {
+            this.splitRoot(node);
+            node = this.root;
+        }
+        while (!node.isLeaf()) {
+            List<K> keys = node.getKeys();
+
+            for (int i = 0; i < keys.size(); ++i) {
+                K k = keys.get(i);
+                if (key.compareTo(k) < 0) {
+                    //as the key inserted smaller than the key in the list
+                    //traverse through left child
+                    List<IBTreeNode<K, V>> children = node.getChildren();
+                    IBTreeNode<K, V> child = children.get(i);
+                    if (child.getNumOfKeys() == maxKeys) {
+                        //split if left child is full
+                        child = split(node, child, key);
+                    }
+                    node = child;
+                    break;
+                } else if (i == keys.size() - 1) {
+                    //traverse through the most right child
+                    List<IBTreeNode<K, V>> children = node.getChildren();
+                    IBTreeNode<K, V> child = children.get(i + 1);
+                    if (child.getNumOfKeys() == maxKeys) {
+                        //split if the most right child is full
+                        child = split(node, child, key);
+                    }
+                    node = child;
+                    break;
+                }
+            }
+
+        }
+
+        return node;
+    }
+
+    //where node is the parent of the child
+    public IBTreeNode<K, V> split(IBTreeNode<K, V> node, IBTreeNode<K, V> child, K key) {
+        IBTreeNode<K, V> leftSplit = new BTreeNode<>();
+        IBTreeNode<K, V> rightSplit = new BTreeNode<>();
+
+        List<K> keys = child.getKeys();
+        List<V> values = child.getValues();
+        List<IBTreeNode<K, V>> children = child.getChildren();
+
+        List<K> newKeys = new LinkedList<>();
+        List<V> newValues = new LinkedList<>();
+        List<IBTreeNode<K, V>> newChildren = new LinkedList<>();
+
+        if (child.isLeaf()) {
+            //assign the new leaves
+            leftSplit.setLeaf(true);
+            rightSplit.setLeaf(true);
+        }
+        //for the left side loop until the middle
+        int i;
+        for (i = 0; i < child.getNumOfKeys() / 2; ++i) {
+            newKeys.add(keys.get(i));
+            newValues.add(values.get(i));
+            if (!child.isLeaf()) {
+                newChildren.add(children.get(i));
+            }
+        }
+
+        if (!child.isLeaf()) {
+            newChildren.add(children.get(i));
+        }
+        leftSplit.setKeys(newKeys);
+        leftSplit.setValues(newValues);
+        leftSplit.setChildren(newChildren);
+        leftSplit.setNumOfKeys(newKeys.size());
+
+        //updating for the new root
+        newKeys = node.getKeys();
+        newValues = node.getValues();
+        newChildren = node.getChildren();
+        K midKey = (keys.get(i));
+
+        int j;
+        for (j = 0; j < newKeys.size() && (keys.get(i)).compareTo(newKeys.get(j)) >= 0; ++j)
+
+
+            newKeys.add(j, midKey);
+        newValues.add(j, values.get(i));
+        newChildren.remove(j);
+        newChildren.add(j, leftSplit);
+        newChildren.add(j + 1, rightSplit);
+        node.setKeys(newKeys);
+        node.setValues(newValues);
+        node.setChildren(newChildren);
+        node.setNumOfKeys(newKeys.size());
+
+        //initialize for the right side
+
+        newKeys = new LinkedList<>();
+        newValues = new LinkedList<>();
+        newChildren = new LinkedList<>();
+
+        ++i;
+        for (; i < child.getNumOfKeys(); ++i) {
+            newKeys.add(keys.get(i));
+            newValues.add(values.get(i));
+            if (!child.isLeaf()) {
+                newChildren.add(children.get(i));
+            }
+        }
+
+        if (!child.isLeaf()) {
+            newChildren.add(children.get(i));
+        }
+        rightSplit.setKeys(newKeys);
+        rightSplit.setValues(newValues);
+        rightSplit.setChildren(newChildren);
+        rightSplit.setNumOfKeys(newKeys.size());
+
+        //if the key greater than the midKey so return the right side
+        if (key.compareTo(midKey) > 0) {
+            return rightSplit;
+        } else {
+            return leftSplit;
+        }
+    }
+
+    public void splitRoot(IBTreeNode<K, V> node) {
+        List<K> keys = node.getKeys();
+        List<V> values = node.getValues();
+        List<IBTreeNode<K, V>> children = node.getChildren();
+
+        IBTreeNode<K, V> newRoot = new BTreeNode<>();
+        IBTreeNode<K, V> leftSplit = new BTreeNode<>();
+        IBTreeNode<K, V> rightSplit = new BTreeNode<>();
+
+        List<K> newKeys = new LinkedList<>();
+        List<V> newValues = new LinkedList<>();
+        List<IBTreeNode<K, V>> newChildren = new LinkedList<>();
+
+        if (node.isLeaf()) {
+            leftSplit.setLeaf(true);
+            rightSplit.setLeaf(true);
+        }
+        //looping through first half of the list to be left side of the split
+
+        int i;
+        for (i = 0; i < node.getNumOfKeys() / 2; ++i) {
+            newKeys.add(keys.get(i));
+            newValues.add(values.get(i));
+            if (!node.isLeaf()) {
+                newChildren.add(children.get(i));
+            }
+        }
+
+        if (!node.isLeaf()) {
+            newChildren.add(children.get(i));
+        }
+        //and update its keys, values, children and number of keys
+        leftSplit.setKeys(newKeys);
+        leftSplit.setValues(newValues);
+        leftSplit.setChildren(newChildren);
+        leftSplit.setNumOfKeys(newKeys.size());
+
+        // initialize again the newKeys, newValues,newChildren to be used for the newRoot
+
+        newKeys = new LinkedList<>();
+        newValues = new LinkedList<>();
+        newChildren = new LinkedList<>();
+
+        //take the middle value and make it the newRoot
+        //update for the newRoot all its keys, values, children and number of keys
+        newKeys.add(keys.get(i));
+        newValues.add(values.get(i));
+        newChildren.add(leftSplit);
+        newChildren.add(rightSplit);
+        newRoot.setKeys(newKeys);
+        newRoot.setValues(newValues);
+        newRoot.setChildren(newChildren);
+        newRoot.setNumOfKeys(newKeys.size());
+
+        // initialize again the newKeys, newValues,newChildren to be used for the right side
+        newKeys = new LinkedList<>();
+        newValues = new LinkedList<>();
+        newChildren = new LinkedList<>();
+
+        //continue looping after the middle to be the right side of the new root
+        ++i;
+        for (; i < node.getNumOfKeys(); ++i) {
+            newKeys.add(keys.get(i));
+            newValues.add(values.get(i));
+            if (!node.isLeaf()) {
+                newChildren.add(children.get(i));
+            }
+        }
+
+        if (!node.isLeaf()) {
+            newChildren.add(children.get(i));
+        }
+
+        rightSplit.setKeys(newKeys);
+        rightSplit.setValues(newValues);
+        rightSplit.setChildren(newChildren);
+        rightSplit.setNumOfKeys(newKeys.size());
+        //at the end this newRoot will be our root
+        this.root = newRoot;
+    }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     @Override
     public V search(K key) {
@@ -103,7 +356,7 @@ public class BTree<K extends Comparable<K>, V> implements IBTree<K, V> {
         IBTreeNode<K, V> subtree = getSubtreeCloseK(root, key);
 
         // if x.ci has only t-1 keys, execute 3a or 3b
-        if (subtree != null && subtree.getNumOfKeys() < t) {
+        if (subtree != null && subtree.getNumOfKeys() < minimumDegree) {
             IBTreeNode<K, V> sibling = getImmediateSibling(root, subtree);
             if (sibling != null) {
                 // CASE 3a:
@@ -155,7 +408,7 @@ public class BTree<K extends Comparable<K>, V> implements IBTree<K, V> {
         return 0;
     }
 
-    private void moveKeys(IBTreeNode<K,V> root, IBTreeNode<K,V> xc, IBTreeNode<K,V> sibling) {
+    private void moveKeys(IBTreeNode<K, V> root, IBTreeNode<K, V> xc, IBTreeNode<K, V> sibling) {
         // right sibling or left sibling
         boolean rightSibling = false;
         int xcIndex = 0;
@@ -249,12 +502,12 @@ public class BTree<K extends Comparable<K>, V> implements IBTree<K, V> {
         for (int i = 0; i < xChildren.size(); i++) {
             if (xChildren.get(i) == subtree) {
                 // Check left sibling with at least t keys
-                if (i > 0 && xChildren.get(i - 1).getNumOfKeys() >= t) {
+                if (i > 0 && xChildren.get(i - 1).getNumOfKeys() >= minimumDegree) {
                     return xChildren.get(i - 1);
                 }
 
                 // Check right sibling with at least t keys
-                if (i < xChildren.size() - 1 && xChildren.get(i + 1).getNumOfKeys() >= t) {
+                if (i < xChildren.size() - 1 && xChildren.get(i + 1).getNumOfKeys() >= minimumDegree) {
                     return xChildren.get(i + 1);
                 }
             }
@@ -285,7 +538,7 @@ public class BTree<K extends Comparable<K>, V> implements IBTree<K, V> {
         // child y that precedes k in node x has at least t keys, then
         // find the predecessor k` of k in the subtree rooted at y
         IBTreeNode<K, V> predecessor = getPredecessor(root, key);
-        if (predecessor != null && predecessor.getNumOfKeys() >= t) {
+        if (predecessor != null && predecessor.getNumOfKeys() >= minimumDegree) {
             K predK = getPredecessorK(predecessor);
             V predV = getPredecessorV(predecessor);
 
@@ -307,7 +560,7 @@ public class BTree<K extends Comparable<K>, V> implements IBTree<K, V> {
         // the child z that follow k in node x, if z has at least t keys, then
         // find the successor k` of k in the subtree rooted at z
         IBTreeNode<K, V> successor = getSuccessor(root, key);
-        if (successor != null && successor.getNumOfKeys() >= t) {
+        if (successor != null && successor.getNumOfKeys() >= minimumDegree) {
             K sucK = getSuccessorK(successor);
             V sucV = getSuccessorV(successor);
 
@@ -436,4 +689,22 @@ public class BTree<K extends Comparable<K>, V> implements IBTree<K, V> {
         }
         return false;
     }
+
+    public void display() {
+        this.display(this.root);
+    }
+
+    private void display(IBTreeNode<K, V> x) {
+        int i;
+        for (i = 0; i < x.getNumOfKeys(); ++i) {
+            System.out.print(x.getKeys().get(i) + " ");
+        }
+
+        if (!x.isLeaf()) {
+            for (i = 0; i < x.getNumOfKeys() + 1; ++i)
+                this.display(x.getChildren().get(i));
+        }
+
+    }
+
 }
